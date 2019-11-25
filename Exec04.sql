d9651a3f0ac1eab204cc311148642fd5f34bf423
Gabriel Carvalho <gabrielmattheus.carvalho@gmail.com>
2019-11-24 23:11:20 -0300

    Update and rename 4.sql to Exec04.sql

--- Exec04.sql
+++ Exec04.sql
@@ -1,14 +1,14 @@
 -- 4)
 
-CREATE OR REPLACE FUNCTION fun(limiter integer) 
+CREATE OR REPLACE FUNCTION procedures_by_delimiter_id(delimiter integer) 
 RETURNS SETOF public.procedure_occurrence 
 AS $fun$
 	BEGIN
 	RETURN QUERY 
 		SELECT * FROM public.procedure_occurrence P
-			WHERE P.person_id <= limiter
+			WHERE P.person_id <= delimiter
 			ORDER BY P.person_id;
 	END;
 $fun$ LANGUAGE plpgsql;
 
-SELECT * FROM fun(100);
+SELECT * FROM procedures_by_delimiter_id(100);
