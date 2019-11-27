37ff337738fb2795b4847c41c9ce7d9a567d721f
victorcezario97 <victorscezario@hotmail.com>
2019-11-26 19:12:03 -0500

    Fix 2.2 and 2.3

--- 2.sql
+++ 2.sql
@@ -7,7 +7,8 @@ FROM
 
 SELECT P.gender_concept_id AS GENERO,
 	( COUNT(*)*100.0 /
-		(SELECT COUNT(*) from public.measurement M2)
+		(SELECT COUNT(*) from public.measurement M2
+		WHERE M2.measurement_date BETWEEN '2008-01-01' AND '2011-12-31')
 	) AS PORCENTAGEM
 	FROM public.measurement M
 	INNER JOIN public.person P ON M.person_id = P.person_id
@@ -16,12 +17,12 @@ SELECT P.gender_concept_id AS GENERO,
 
 SELECT P.gender_concept_id AS GENERO,
 	( COUNT(*)*100.0 /
-		(SELECT COUNT(*) from public.measurement)
+		(SELECT COUNT(*) from public.measurement M2 
+		WHERE M2.measurement_date BETWEEN '2008-01-01' AND '2011-12-31')
 	) AS PORCENTAGEM,
 	EXTRACT(YEAR FROM M.measurement_date)
 	FROM public.measurement M
 	INNER JOIN public.person P ON M.person_id = P.person_id
 	WHERE M.measurement_date BETWEEN '2008-01-01' AND '2011-12-31'
 	GROUP BY P.gender_concept_id, EXTRACT(YEAR FROM M.measurement_date)
-	ORDER BY EXTRACT(YEAR FROM M.measurement_date), P.gender_concept_id;
-		
+	ORDER BY EXTRACT(YEAR FROM M.measurement_date), P.gender_concept_id;	
