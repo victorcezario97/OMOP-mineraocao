63166a26d5b168b51de92c344753697cb0d99c3d
Gabriel Carvalho <gabrielmattheus.carvalho@gmail.com>
2019-11-26 19:00:52 -0500

    Update 1.sql

--- 1.sql
+++ 1.sql
@@ -10,14 +10,23 @@ Count(*) as QUANTIDADE
 
 
 -- Equi-largura
-SELECT Floor(M.measurement_concept_id/5.00)*5 as ID, Count(*) as QUANTIDADE
-	FROM public.measurement M
-	TABLESAMPLE BERNOULLI (.15)
-	INNER JOIN public.concept C on M.measurement_concept_id = C.concept_id
-	GROUP BY ID
-	ORDER BY ID;
+SELECT Bins.B AS ID,
+	CASE WHEN Tab.Conta IS NULL THEN 0
+		ELSE Tab.Conta END
+	FROM 
+		(WITH Lim AS (
+			SELECT Min(M.measurement_concept_id) Mi, Max(M.measurement_concept_id) Ma
+				FROM public.measurement M)
+			SELECT Generate_Series(Lim.Mi + 1, Lim.Ma - 1, 100000) 
+				AS B FROM Lim) AS Bins
+				LEFT OUTER JOIN
+			(SELECT Floor(M1.measurement_concept_id / 100000)*100000 AS ID, Count(*) AS CONTA
+				FROM public.measurement M1
+				TABLESAMPLE BERNOULLI (.15)
+				GROUP BY ID) as Tab
+					ON (Bins.B <= Tab.ID AND Tab.ID < (Bins.B + 100000));
 
-- Equi-altura
+-- Equi-altura
 SELECT Bin, Min(measurement_concept_id), Max(measurement_concept_id), Count(*)
 	FROM (SELECT *, NTILE(10) OVER(ORDER BY measurement_concept_id) AS Bin
 	FROM (SELECT * FROM measurement TABLESAMPLE BERNOULLI (.15) ) AS Sample) AS Partes 
